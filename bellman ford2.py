11be9a053190d8e8f6823218ccdf4082dc515480
Welly Wong <70742141+WellyWong@users.noreply.github.com>
2021-09-12 22:43:17 -0500

    Update bellman ford2.py

--- bellman ford2.py
+++ bellman ford2.py
@@ -2,10 +2,6 @@
 The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other
 vertices in a weighted digraph. It is slower than Dijkstra's algorithm for the same problem, but more versatile,
 as it is capable of handling graphs in which some of the edge weights are negative numbers.
-
-The Bellman–Ford algorithm is an algorithm that computes shortest paths from a single source vertex to all of the other
-vertices in a weighted digraph. It is slower than Dijkstra's algorithm for the same problem, but more versatile,
-as it is capable of handling graphs in which some of the edge weights are negative numbers.
 Negative edge weights are found in various applications of graphs, hence the usefulness of this algorithm. If a graph
 contains a "negative cycle" (i.e. a cycle whose edges sum to a negative value) that is reachable from the source, then
 there is no cheapest path: any path that has a point on the negative cycle can be made cheaper by one more walk
@@ -68,7 +64,6 @@ d, p = bellman_ford(graph, 'a')
 print(d)
 print(p)
 print(graph)
-print(float('Inf'))
 
 g = {'S': {'A': 10, 'E': 8}, 'A': {'C': 2}, 'B': {'A': 1}, 'E': {'D': 1}, 'D': {'C': -1, 'A': -4}, 'C': {'B': -2}}
 d, p = bellman_ford(g, 'S')
